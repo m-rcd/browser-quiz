f0998b9e0757415b83de0b8ee14bdb66e480dfe2
Marianne Rachid <marianne.rachid@gmail.com>
2018-09-27 12:38:38 +0100

    add test for .all

--- spec/units/questions_spec.rb
+++ spec/units/questions_spec.rb
@@ -1,16 +1,18 @@
 require './lib/questions'
 
 describe Questions do
-  describe 'random_question' do
+  describe '.all' do
     let(:questions) {described_class.new}
-     # it 'returns a random question' do
-     #   connection = PG.connect :dbname => 'hp_test'
-     #
-     #   connection.exec("INSERT INTO questions_test VALUES('Which Harry Potter word is now in the Oxford English Dictionary?');")
-     #   connection.exec("INSERT INTO questions_test VALUES('What is  the Animagus form of Rita Skeeter?');")
-     #   connection.exec("INSERT INTO questions_test VALUES('How many names does Dumbledore have in his full name?');")
-     #   srand(123)
-     #  expect(questions.random_question).to eq('How many names does Dumbledore have in his full name?')
-    # end
+    it 'returns a list of questions' do
+      connection = PG.connect :dbname => 'hp_test'
+
+        connection.exec("INSERT INTO questions_test VALUES('Which Harry Potter word is now in the Oxford English Dictionary?');")
+        connection.exec("INSERT INTO questions_test VALUES('What is  the Animagus form of Rita Skeeter?');")
+        connection.exec("INSERT INTO questions_test VALUES('How many names does Dumbledore have in his full name?');")
+        questions = Questions.all
+        expect(questions).to include('Which Harry Potter word is now in the Oxford English Dictionary?')
+        expect(questions).to include('What is  the Animagus form of Rita Skeeter?')
+        expect(questions).to include('How many names does Dumbledore have in his full name?')
+    end
   end
 end
